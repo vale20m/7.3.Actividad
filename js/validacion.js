eacd6b51bb616937f4dccbef134de96207b56a46
Vale20m <valentinveintemilla@gmail.com>
2023-08-09 18:55:17 -0300

    Agregamos las funcionalidades requeridas

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,32 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+// Verificar que los campos no esten vacios y que las contraseñas sean iguales
+
+const contraseña1 = document.getElementById("password1");
+const contraseña2 = document.getElementById("password2");
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const terminos = document.getElementById("terminos");
+
+function verificarCampos(){
+    if (nombre.value != "" && apellido.value != "" && email.value != "" && contraseña1.value != "" && contraseña2.value != "" && contraseña1.value == contraseña2.value && terminos.checked){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Verificar que los datos sean válidos
+
+const registrarme = document.getElementById("regBtn");
+
+registrarme.addEventListener("click", function(){
+    if (verificarCampos()){
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    }
+});
